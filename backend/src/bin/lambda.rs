59ae1caeb051729033e23afe4cb81c000e428e55
Carsten Kragelund <carsten@kragelund.dev>
2021-03-30 22:28:25 +0200

    Change License, and package names for frontend and backend

--- backend/src/bin/lambda.rs
+++ backend/src/bin/lambda.rs
@@ -1,8 +1,8 @@
 // Please change to the name of the project in the Cargo.toml
-use backend_server;
+use orr_server;
 use rocket_lamb::RocketExt;
 
 #[tokio::main]
 async fn main() {
-    backend_server::rocket().lambda().launch().await;
+    orr_server::rocket().lambda().launch().await;
 }
--- backend/src/bin/main.rs
+++ backend/src/bin/main.rs
@@ -1,7 +1,7 @@
 // Please change to the name of the project in the Cargo.toml
-use backend_server;
+use orr_server;
 
 #[tokio::main]
 async fn main() {
-    backend_server::rocket().launch().await.unwrap();
+    orr_server::rocket().launch().await.unwrap();
 }
