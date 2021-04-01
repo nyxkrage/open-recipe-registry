7068984735252d7ba8abe23831485f0780387ea4
Carsten Kragelund <carsten@kragelund.dev>
2021-04-02 00:36:49 +0200

    Fix MPA

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -1,111 +1,147 @@
-import svelte from 'rollup-plugin-svelte';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
-import alias from "@rollup/plugin-alias";
-import copy from "rollup-plugin-copy";
-const { readdirSync } = require('fs')
+import copy from 'rollup-plugin-copy'
+import alias from '@rollup/plugin-alias';
+import { readdirSync } from 'fs';
 
 const getDirectories = source =>
   readdirSync(source, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name.replace(__dirname, ""))
 
-const pages = getDirectories(__dirname + "/src/pages").concat([""]);
+const inputs = getDirectories(__dirname + "/src/pages")
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
-	let server;
+  let server;
 
-	function toExit() {
-		if (server) server.kill(0);
-	}
+  function toExit() {
+    if (server) server.kill(0);
+  }
 
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
+  return {
+    writeBundle() {
+      if (server) return;
+      server = require("child_process").spawn(
+        "npm",
+        ["run", "start", "--", "--dev"],
+        {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true,
+        }
+      );
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
+      process.on("SIGTERM", toExit);
+      process.on("exit", toExit);
+    },
+  };
+}
+function createPageRollupExport(inp) {
+  //nearly default config as in https://github.com/sveltejs/template
+  //TODO add possibilty for different option with different inputs and destinations.
+  const input = `src/pages/${inp}/${inp}.ts`;
+  const output = {
+    sourcemap: false,
+    format: "iife",
+    name: `${inp}`,
+    file: `public/build/${inp}.js`,
+  };
+  const cssPath = `${inp}.css`;
+
+  let def = {
+    input: input,
+    output: output,
+    plugins: [
+		svelte({
+			preprocess: sveltePreprocess({ sourceMap: !production }),
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production,
+			},
+		}),
+		alias({
+			entries: [
+				// If you add a new top-level-folder besides src which you want to use, add it here
+				{ find: /^src(\/|$)/, replacement: `${__dirname}/src/` },
+			],
+		}),
+		copy({
+			targets: [
+				{ 
+				src: 'src/index.html',
+				dest: `public`,
+				rename: (_, extension) => `${inp === "main" ? "index" : inp}.${extension}`,
+				transform: (contents) => contents.toString().replace(/__name__/g, inp),
+				},
+			]
+		}),
+		css({ output: cssPath }),
+
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: true,
+			dedupe: ["svelte"],
+		}),
+		commonjs(),
+		typescript({
+			sourceMap: !production,
+			inlineSources: !production
+		}),
+
+		// In dev mode, call `npm run start` once
+		// the bundle has been generated
+		inp === "main" && !production && serve(),
+
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		inp === "main" && !production && livereload("public"),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser(),
+    ],
+    watch: {
+		clearScreen: false,
+    },
+  };
+
+  return def;
 }
 
-const common = function (dir, page) {
-	let isRoot = page === "";
-	return {
-		input: `src${isRoot ? "" : "/pages/"}${page}/main.ts`,
-		output: {
-			sourcemap: true,
-			format: 'iife',
-			name: 'app',
-			file: dir + page + "/bundle.js"
-		},
-		plugins: [
-			copy({
-				targets: [
-				  { src: "src/index.html", dest: dir + page },
-				  { src: "src/global.css", dest: dir + page },
-				],
-			}),
-			svelte({
-				preprocess: sveltePreprocess({ sourceMap: !production }),
-				compilerOptions: {
-					// enable run-time checks when not in production
-					dev: !production
-				}
-			}),
-			alias({
-				entries: [
-					// If you add a new top-level-folder besides src which you want to use, add it here
-					{ find: /^src(\/|$)/, replacement: `${__dirname}/src/` },
-				],
-			}),
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css({ output: 'bundle.css' }),
-			
-			// If you have external dependencies installed from
-			// npm, you'll most likely need these plugins. In
-			// some cases you'll need additional configuration -
-			// consult the documentation for details:
-			// https://github.com/rollup/plugins/tree/master/packages/commonjs
-			resolve({
-				browser: true,
-				dedupe: ['svelte']
-			}),
-			commonjs(),
-			typescript({
-				sourceMap: !production,
-				inlineSources: !production
-			}),
-			
-			// In dev mode, call `npm run start` once
-			// the bundle has been generated
-			page === "" && !production && serve(),
-				
-			// Watch the `public` directory and refresh the
-			// browser on changes when not in production
-			page === "" && !production && livereload('public'),
-			
-			// If we're building for production (npm run build
-			// instead of npm run dev), minify
-			production && terser()
-		],
-		watch: {
-			clearScreen: false
-		}
-	}
-};
+function validateInputSettings(inp) {
+  return (
+    isStringNotNull(inp) ||
+    (typeof inp === "object" && inp.input && inp.output && inp.css)
+  );
+}
 
-const exp = pages.map((folder) => common("public/", folder));
+function isStringNotNull(str) {
+  return typeof str === "string" && str.length > 0;
+}
 
-export default exp;
+export default (function () {
+  let arrExportRollup = [];
+
+  if (inputs instanceof Array) {
+    inputs.forEach((i, index) => {
+      if (validateInputSettings(i)) {
+        arrExportRollup.push(createPageRollupExport(i));
+      } else {
+        console.warn(`skipping inputs${index} because of invalidity`);
+      }
+    });
+  } else {
+    throw new Error("inputs needs to be type of Array!");
+  }
+  return arrExportRollup;
+})();
---
+++ frontend/src/components/Card.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+
+<card>
+    <img src="imgsrc" alt="recipetitle"/>
+    <span class="title">Title of recipe</span>
+    <ul><li>nationality</li><li>ingredient 1</li><li>ingredient 2</li></ul>
+    
+</card>
+
+<style lang="scss">
+
+</style>
